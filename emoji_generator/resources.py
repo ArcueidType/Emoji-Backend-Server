deae1ea0c4e92d907876a4f276fc1f372336fdf3
ArcueidType <981354012@qq.com>
2024-07-11 02:07:56 +0800

    complete the graywordmeme

--- emoji_generator/resources.py
+++ emoji_generator/resources.py
@@ -1,5 +1,10 @@
 from flask_restful import Resource
 from flask import request
+import base64
+import numpy as np
+import cv2
+from PIL import Image, ImageFont, ImageDraw
+from io import BytesIO
 
 
 class AddOp(Resource):
@@ -9,3 +14,40 @@ class AddOp(Resource):
         num2 = data['num2']
         result = num1 + num2
         return {'code': 200, 'msg': 'ok', 'result': result}
+
+
+class GrayWordMeme(Resource):
+    def post(self):
+        try:
+            data = request.get_json()
+            img = data['img']
+            text = data['text']
+
+            img = img.encode('ascii')
+            img = base64.b64decode(img)
+            img_arr = np.frombuffer(img, dtype=np.uint8)
+            img = cv2.imdecode(img_arr, cv2.IMREAD_COLOR)
+
+            height, width, _ = img.shape
+            img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+            img = cv2.copyMakeBorder(img, 0, round(height / 5), 0, 0, cv2.BORDER_CONSTANT, value=[0, 0, 0])
+            img = cv2.cvtColor(img, cv2.COLOR_GRAY2RGB)
+
+            img = Image.fromarray(img)
+            size = int((height / 5) * 0.75)
+            font = ImageFont.truetype(r'./GlowSansSC-Normal-Heavy.otf', size)
+
+            txt_width, txt_height = font.getsize(text)
+
+            draw = ImageDraw.Draw(img)
+            draw.text(((width - txt_width) / 2, height + (height // 5 - txt_height) / 2), text, fill='white', font=font)
+
+            buf = BytesIO()
+            img.save(buf, 'PNG')
+            ret_img = buf.getvalue()
+            ret_img = base64.b64encode(ret_img)
+            ret_img = ret_img.decode('ascii')
+
+            return {'code': 200, 'msg': 'ok', 'result': ret_img}
+        except Exception as e:
+            return {'code': 500, 'msg': f'Process Procedure Error: {e}'}
--- test.py
+++ test.py
@@ -1,19 +1,51 @@
-from PIL import Image, ImageDraw, ImageFont
+# from PIL import Image, ImageDraw, ImageFont
+#
+# size = 50
+# txt = 'hello'
+# img = Image.open('1.jpg')
+# font = ImageFont.truetype(r'Deng.ttf', size)
+# h, w = img.size
+#
+# l, t, r, b = font.getbbox(txt)
+# w1 = r - l
+# h1 = b - t
+# draw = ImageDraw.Draw(img)
+# l, t, r, b = draw.textbbox((0, 0), text=txt, font=font)
+# w1 = r - l
+# h1 = b - t
+#
+# draw.text(((w-w1)/2, h/2), txt, fill='white', font=font)
+# # draw.text((5, 5), txt, fill='red', font=font)
+# img.show()
 
-size = 50
-txt = 'hello'
-img = Image.open('1.jpg')
-font = ImageFont.truetype(r'Deng.ttf', size)
-h, w = img.size
+import requests
+import json
+from io import BytesIO
+import base64
+import numpy as np
+import cv2
+from PIL import Image
 
-l, t, r, b = font.getbbox(txt)
-w1 = r - l
-h1 = b - t
-draw = ImageDraw.Draw(img)
-l, t, r, b = draw.textbbox((0, 0), text=txt, font=font)
-w1 = r - l
-h1 = b - t
+with open('1.jpg', 'rb') as f:
+    img_data = f.read()
 
-draw.text(((w-w1)/2, h/2), txt, fill='white', font=font)
-# draw.text((5, 5), txt, fill='red', font=font)
+
+img_base = base64.b64encode(img_data)
+img = img_base.decode('ascii')
+
+headers = {'Content-Type': 'application/json'}
+data = json.dumps({'img': img, 'text': '我补药玩CS'})
+
+response = requests.post('http://127.0.0.1:5000/graywordmeme', headers=headers, data=data)
+
+img = response.json().get('result')
+
+img = img.encode('ascii')
+img = base64.b64decode(img)
+
+img_arr = np.frombuffer(img, dtype=np.uint8)
+img = cv2.imdecode(img_arr, cv2.IMREAD_COLOR)
+img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+img = Image.fromarray(img)
 img.show()
+
